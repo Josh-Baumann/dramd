54f5cff80f891d6a1bc8b35730aa24c99fc3c6c9
Josh Baumann <61565858+Josh-Baumann@users.noreply.github.com>
2024-09-28 18:46:17 -0400

    Update home page layout

--- dramd/dramd.py
+++ dramd/dramd.py
@@ -3,6 +3,8 @@
 import reflex as rx
 
 from rxconfig import config
+from .views.navbar import navbar
+from .views.sidebar import sidebar_bottom_profile
 
 
 class State(rx.State):
@@ -13,27 +15,50 @@ class State(rx.State):
 
 def index() -> rx.Component:
     # Welcome Page (Index)
-    return rx.container(
-        rx.color_mode.button(position="top-right"),
-        rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
-            rx.text(
-                "Get started by editing ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
-                size="5",
+    return rx.vstack(
+        rx.box(
+            rx.hstack(
+                sidebar_bottom_profile(),
+                rx.hstack(
+                    rx.badge(
+                        rx.icon(tag="glass-water", size=28),
+                        rx.heading(
+                            "dramd",
+                            size="6",
+                            justify="end",
+                        ),
+                        variant="surface",
+                        radius="large",
+                    ),
+                    padding="0.5rem",
+                ),
+                align_items="center",
+                width="100%",
+                justify="between",
             ),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
-            ),
-            spacing="5",
-            justify="center",
-            min_height="85vh",
+            bg=rx.color("accent", 3),
+            width="100%",
         ),
-        rx.logo(),
+        rx.container(
+            rx.vstack(
+                rx.heading("Welcome to dramd!", size="9"),
+                rx.text(
+                    "Get started by editing ",
+                    rx.code(f"{config.app_name}/{config.app_name}.py"),
+                    size="5",
+                ),
+                rx.link(
+                    rx.button("Check out our docs!"),
+                    href="https://reflex.dev/docs/getting-started/introduction/",
+                    is_external=True,
+                ),
+                spacing="5",
+                justify="top",
+                min_height="85vh",
+            ),
+        ),
     )
 
 
-app = rx.App()
+app = rx.App(theme=rx.theme(appearance="dark", accent_color="purple"))
 app.add_page(index)
